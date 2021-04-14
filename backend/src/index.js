e265a06fc1f88c4e68a7da4442e667ecef75b785
Josenildo Coutinho Silva Lins <josenildo_004@hotmail.com>
2021-04-14 19:38:55 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,14 +3,10 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 
  const app = express(); //coloca a aplicação no ar
- mongoose.connect('mongodb+srv://josenildolins:123123123@cluster0-u7nls.mongodb.net/semana1?retryWrites=true&w=majority', {
+ mongoose.connect( {
     useNewUrlParser: true,
     useUnifiedTopology : true,
  });
  app.use(express.json());
  app.use(routes);
-
-
-
-
  app.listen(3333);
